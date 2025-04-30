97d3562221a21cdb50bd587df932c1b5a5c6341c
Santiago Acosta <santiacosta2499@gmail.com>
2025-04-30 19:42:23 -0300

    Cambiando estilo de la pagina

--- src/components/product_catalog.jsx
+++ src/components/product_catalog.jsx
@@ -4,7 +4,6 @@ import ProductCard from './product_card';
 function ProductCatalog({ products, addToCart }) {
   return (
     <div className="products">
-      <h1>Food App</h1>
       <div className="productList">
         {products.map(product => (
           <ProductCard
